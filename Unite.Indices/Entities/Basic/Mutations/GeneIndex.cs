f81ffb65329f66753980052885ca5146d98f9458
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-03-04 19:47:09 +0100

    Changed position to start and end. Temporarily removed gene index.

--- Unite.Indices/Entities/Basic/Mutations/GeneIndex.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Unite.Indices.Entities.Basic.Mutations
-{
-    public class GeneIndex
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Unite.Indices/Entities/Basic/Mutations/MutationIndex.cs
+++ Unite.Indices/Entities/Basic/Mutations/MutationIndex.cs
@@ -3,16 +3,13 @@
     public class MutationIndex
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public string Code { get; set; }
         public string Chromosome { get; set; }
-        public string Contig { get; set; }
         public string SequenceType { get; set; }
-        public int Position { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
         public string Type { get; set; }
         public string Ref { get; set; }
         public string Alt { get; set; }
-
-        public GeneIndex Gene { get; set; }
     }
 }
