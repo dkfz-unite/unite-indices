c8ff09cb20382d02350ef17d855690c3ea169fea
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-05-25 16:44:58 +0200

    Revert "Moved reference identifier from specimen to specific specimen indices to allow identical identifiers across different types of specimens."
    This reverts commit 094cf38c2aaab5eefc2a262f80a1b1e03294cb22.

--- Unite.Indices/Entities/Basic/Specimens/CellLineIndex.cs
+++ Unite.Indices/Entities/Basic/Specimens/CellLineIndex.cs
@@ -4,7 +4,6 @@ namespace Unite.Indices.Entities.Basic.Specimens
 {
     public class CellLineIndex
     {
-        public string ReferenceId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Species { get; set; }
