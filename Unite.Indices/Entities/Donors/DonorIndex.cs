0cecc327c4e0c662683fe166052d57cca2594db5
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-06-12 09:35:50 +0200

    Adding number of specimens to donor index.

--- Unite.Indices/Entities/Donors/DonorIndex.cs
+++ Unite.Indices/Entities/Donors/DonorIndex.cs
@@ -5,6 +5,11 @@
         public MutationIndex[] Mutations { get; set; }
 
         /// <summary>
+        /// Number of donor specimens
+        /// </summary>
+        public int NumberOfSpecimens { get; set; }
+
+        /// <summary>
         /// Number of donor mutations
         /// </summary>
         public int NumberOfMutations { get; set; }
