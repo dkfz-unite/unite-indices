fb1f89fce3d32b66fb04a16a7a57cc599dd230f6
vulasau <valiantsin.ulasau@gmail.com>
2022-03-08 11:45:44 +0100

    Updated image index model.

--- Unite.Indices/Entities/Images/ImageIndex.cs
+++ Unite.Indices/Entities/Images/ImageIndex.cs
@@ -2,8 +2,25 @@
 {
     public class ImageIndex : Basic.Images.ImageIndex
     {
+        /// <summary>
+        /// Donor of the image
+        /// </summary>
         public DonorIndex Donor { get; set; }
 
+        /// <summary>
+        /// Image donor tissues
+        /// </summary>
         public SpecimenIndex[] Specimens { get; set; }
+
+
+        /// <summary>
+        /// Total number of genes affected by any mutation across all tissues of the image donor
+        /// </summary>
+        public int NumberOfGenes { get; set; }
+
+        /// <summary>
+        /// Total number of mutations across all tissues of the image donor
+        /// </summary>
+        public int NumberOfMutations { get; set; }
     }
 }
