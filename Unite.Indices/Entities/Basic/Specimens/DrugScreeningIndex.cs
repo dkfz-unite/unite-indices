c72263220db1d49e030c098baf1aad7adfdf7e57
vulasau <valiantsin.ulasau@gmail.com>
2022-07-26 14:42:31 +0200

    Extended drug screening data.

--- Unite.Indices/Entities/Basic/Specimens/DrugScreeningIndex.cs
+++ Unite.Indices/Entities/Basic/Specimens/DrugScreeningIndex.cs
@@ -3,11 +3,13 @@
 public class DrugScreeningIndex
 {
     public string Drug { get; set; }
-    public double? MinConcentration { get; set; }
-    public double? MaxConcentration { get; set; }
     public double? Dss { get; set; }
     public double? DssSelective { get; set; }
+    public double? Gof { get; set; }
+    public double? MinConcentration { get; set; }
+    public double? MaxConcentration { get; set; }
     public double? AbsIC25 { get; set; }
     public double? AbsIC50 { get; set; }
     public double? AbsIC75 { get; set; }
+    public double[] Inhibition { get; set; }
 }
