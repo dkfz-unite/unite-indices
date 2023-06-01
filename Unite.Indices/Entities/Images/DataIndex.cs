05ff27e3101ad345a5fda2b573af0e26e1da43d7
vulasau <valiantsin.ulasau@gmail.com>
2023-06-01 12:38:01 +0200

    Removed specimen availability properties from image data index.

--- Unite.Indices/Entities/Images/DataIndex.cs
+++ Unite.Indices/Entities/Images/DataIndex.cs
@@ -1,11 +1,7 @@
 namespace Unite.Indices.Entities.Images;
 
-public class DataIndex : Basic.ISpecimensDataIndex, Basic.IGenomeDataIndex
+public class DataIndex : Basic.IGenomeDataIndex
 {
-    public bool Tissues { get; set; }
-    public bool Cells { get; set; }
-    public bool Organoids  { get; set; }
-    public bool Xenografts { get; set; }
     public bool SSMs { get; set; }
     public bool CNVs { get; set; }
     public bool SVs { get; set; }
