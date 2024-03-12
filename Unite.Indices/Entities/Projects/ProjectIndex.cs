21244c2ea3dc57a7ff9dcb49f1291bb44ea4b699
DGitRich <daniel.rich@gmx.net>
2024-03-12 17:26:36 +0100

    Update ProjectIndex

--- Unite.Indices/Entities/Projects/ProjectIndex.cs
+++ Unite.Indices/Entities/Projects/ProjectIndex.cs
@@ -8,8 +8,21 @@ public class ProjectIndex : Basic.Donors.ProjectIndex
     public int NumberOfLines { get; set; }
     public int NumberOfOrganoids { get; set; }
     public int NumberOfXenografts { get; set; }
+
+    // Still necessary with more detailed genes counters?
     public int NumberOfSsms { get; set; }
     public int NumberOfCnvs { get; set; }
     public int NumberOfSvs { get; set; }
+    // --------------------------------------------------
+
     public int NumberOfTranscriptomics { get; set; }
+
+    // Drafts
+    public Dictionary<string, int> NumberOfDonorsByDiagnosis { get; set; } // Everything with empty values goes to "Other"
+    public Dictionary<string, int> NumberOfDonorsByGender { get; set; }
+    public Dictionary<int, int> NumberOfDonorsByAge { get; set; }
+    public Dictionary<string, int> GenesAffectedBySsms { get; set; }
+    public Dictionary<string, int> GenesAffectedByCnvs { get; set; }
+    public Dictionary<string, int> GenesAffectedBySvs { get; set; }
+
 }
