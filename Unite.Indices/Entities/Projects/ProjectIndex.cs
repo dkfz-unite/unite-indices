7b63ca702d3572a12d3425d73d4a19741bdd83c9
DGitRich <daniel.rich@gmx.net>
2024-03-13 14:47:06 +0100

    Update ProjectIndex

--- Unite.Indices/Entities/Projects/ProjectIndex.cs
+++ Unite.Indices/Entities/Projects/ProjectIndex.cs
@@ -3,26 +3,21 @@ namespace Unite.Indices.Entities.Projects;
 public class ProjectIndex : Basic.Donors.ProjectIndex
 {
     public int NumberOfDonors { get; set; }
+    public Dictionary<string, int> NumberOfDonorsByGender { get; set; }
+    public Dictionary<int, int> NumberOfDonorsByAge { get; set; }
+    public Dictionary<string, int> NumberOfDonorsByDiagnosis { get; set; }
+    public double PercentOfDonorsVital { get; set; }
+    public double PercentOfDonorsProgressing { get; set; }
     public int NumberOfMriImages { get; set; }
     public int NumberOfMaterials { get; set; }
     public int NumberOfLines { get; set; }
     public int NumberOfOrganoids { get; set; }
     public int NumberOfXenografts { get; set; }
-
-    // Still necessary with more detailed genes counters?
     public int NumberOfSsms { get; set; }
-    public int NumberOfCnvs { get; set; }
-    public int NumberOfSvs { get; set; }
-    // --------------------------------------------------
-
-    public int NumberOfTranscriptomics { get; set; }
-
-    // Drafts
-    public Dictionary<string, int> NumberOfDonorsByDiagnosis { get; set; } // Everything with empty values goes to "Other"
-    public Dictionary<string, int> NumberOfDonorsByGender { get; set; }
-    public Dictionary<int, int> NumberOfDonorsByAge { get; set; }
     public Dictionary<string, int> GenesAffectedBySsms { get; set; }
+    public int NumberOfCnvs { get; set; }
     public Dictionary<string, int> GenesAffectedByCnvs { get; set; }
+    public int NumberOfSvs { get; set; }
     public Dictionary<string, int> GenesAffectedBySvs { get; set; }
-
+    public int NumberOfTranscriptomics { get; set; }
 }
