986f0d79af433a8a32534b8a559addac31070004
DGitRich <daniel.rich@gmx.net>
2024-03-18 15:34:33 +0100

    Create SpecimenStatsIndex

---
+++ Unite.Indices/Entities/Projects/SpecimenStatsIndex.cs
@@ -0,0 +1,9 @@
+namespace Unite.Indices.Entities.Projects;
+
+public class SpecimenStatsIndex
+{
+    public int NumberOfMaterials { get; set; }
+    public int NumberOfLines { get; set; }
+    public int NumberOfOrganoids { get; set; }
+    public int NumberOfXenografts { get; set; }
+}
