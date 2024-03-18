3e8a0fa6f13942f83d98590cb9ba5e7479c0e0d6
DGitRich <daniel.rich@gmx.net>
2024-03-18 15:35:50 +0100

    Create VariantStatsIndex

---
+++ Unite.Indices/Entities/Projects/VariantStatsIndex.cs
@@ -0,0 +1,13 @@
+using Unite.Indices.Models;
+
+namespace Unite.Indices.Entities.Projects;
+
+public class VariantStatsIndex
+{
+    public int NumberOfSsms { get; set; }
+    public Stat[] GenesAffectedBySsms { get; set; }
+    public int NumberOfCnvs { get; set; }
+    public Stat[] GenesAffectedByCnvs { get; set; }
+    public int NumberOfSvs { get; set; }
+    public Stat[] GenesAffectedBySvs { get; set; }
+}
