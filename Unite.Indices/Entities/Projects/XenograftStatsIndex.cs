80aa82243e3c768b35c242983b9a1a707d02ffc6
DGitRich <daniel.rich@gmx.net>
2024-05-02 10:45:25 +0200

    Update XenograftStatsIndex

--- Unite.Indices/Entities/Projects/XenograftStatsIndex.cs
+++ Unite.Indices/Entities/Projects/XenograftStatsIndex.cs
@@ -5,5 +5,5 @@ public class XenograftStatsIndex
     public int Total { get; set; }
     public Stat<int>[] ByDrugDss { get; set; }
     public Stat<int>[] ByDrugSdss { get; set; }
-    public Stat<int>[] ByInterventionsType { get; set; }
+    public Stat<int>[] ByInterventionType { get; set; }
 }
