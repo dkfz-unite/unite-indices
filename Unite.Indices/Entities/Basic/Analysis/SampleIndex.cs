91fe6d5eb5480e05ebb7a056ae9a8d838ab2e19f
vulasau <valiantsin.ulasau@gmail.com>
2024-08-08 11:13:27 +0200

    Changed sample fields. New Versions.

--- Unite.Indices/Entities/Basic/Analysis/SampleIndex.cs
+++ Unite.Indices/Entities/Basic/Analysis/SampleIndex.cs
@@ -5,9 +5,9 @@ public class SampleIndex
     public int Id { get; set; }    
     public string AnalysisType { get; set; }
     public int? AnalysisDay { get; set; }
+    public string Genome { get; set; }
 
     public double? Purity { get; set; }
     public double? Ploidy { get; set; }
-    public int? CellsNumber { get; set; }
-    public string GenesModel { get; set; }
+    public int? Cells { get; set; }   
 }
