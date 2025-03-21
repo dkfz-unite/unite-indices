f6c25e35f2eb079f81bc17d8399d99421af49e93
vulasau <valiantsin.ulasau@gmail.com>
2025-03-21 15:30:30 +0100

    Updated resource index.

--- Unite.Indices/Entities/Basic/Analysis/ResourceIndex.cs
+++ Unite.Indices/Entities/Basic/Analysis/ResourceIndex.cs
@@ -4,6 +4,7 @@ public class ResourceIndex
 {
     public int Id { get; set; }
     
+    public string Name { get; set; }
     public string Type { get; set; }
     public string Format { get; set; }
     public string Archive { get; set; }
