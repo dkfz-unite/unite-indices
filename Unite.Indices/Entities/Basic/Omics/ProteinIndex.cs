39f1966b064a1e6e25ab7711107e0006f5e3adfb
vulasau <valiantsin.ulasau@gmail.com>
2026-01-30 14:48:23 +0100

    New protein fields.

--- Unite.Indices/Entities/Basic/Omics/ProteinIndex.cs
+++ Unite.Indices/Entities/Basic/Omics/ProteinIndex.cs
@@ -4,6 +4,10 @@ public class ProteinIndex
 {
     public int Id { get; set; }
     public string StableId { get; set; }
+    public string AccessionId { get; set; }
+    public string Database { get; set; }
+    public string Symbol { get; set; }
+    public string Description { get; set; }
     public bool? IsCanonical { get; set; }
     public int? Start { get; set; }
     public int? End { get; set; }
